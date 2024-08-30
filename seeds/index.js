7c13bbac4e48ae14bd8746d8099dc928c0c652db
gautam56863 <gautamaggarwal133@gmail.com>
2024-08-31 00:48:33 +0530

    Bootstrap -navbar -footer -ejs-mate

--- seeds/index.js
+++ seeds/index.js
@@ -13,15 +13,20 @@ mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
     console.log(err);
 })
 
+
 const sample = array => array[Math.floor(Math.random() * array.length)]
 
 const seedDb = async () => {
     await Campground.deleteMany({});
     for( let i = 0 ; i<50 ; i++){
         const rand1000 = Math.floor(Math.random()*1000);
+        const price = Math.floor(Math.random()*20)+10;
         const camp= new Campground({
             location : `${cities[rand1000].city}, ${cities[rand1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image:`https://picsum.photos/400?random=${Math.random}`,
+            description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis magnam possimus, error iste porro vero quaerat voluptate voluptates. Natus facilis quidem nihil obcaecati earum voluptates laudantium rem ullam. Consectetur, veniam.',
+            price
         })
         await camp.save();
     }
